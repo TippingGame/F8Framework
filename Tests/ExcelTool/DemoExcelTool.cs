05cec4c705e6bd0c75f0060ae6dcdd760b730402
TippingGame <tippinggame@163.com>
2024-06-21 00:13:11 +0800

    1.0.17 新增F8Framework.F8ExcelDataClass和F8Framework.Launcher程序集，配置表生成的F8Framework.F8ExcelDataClass可用于HybridCLR热更，增加导表后脚本编译完成，再打包AB步骤，重新加载C#域后，AssetDatabase.Refresh需要增加delayCall延迟执行，完善热更文档，F8Framework.Tests程序集增加宏定义不参与打包

--- Tests/ExcelTool/DemoExcelTool.cs
+++ Tests/ExcelTool/DemoExcelTool.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using F8Framework.Core;
-// using F8Framework.F8ExcelDataClass;
+using F8Framework.F8ExcelDataClass;
+using F8Framework.Launcher;
 
 namespace F8Framework.Tests
 {
     public class DemoExcelTool : MonoBehaviour
     {
         // 方式一：读取二进制或者json
-        // IEnumerator Start()
-        // {
-        //     // 同步加载全部配置
-        //     FF8.Config.LoadAll();
-        //
-        //     foreach (var item in FF8.Config.LoadAllAsync()) // 异步加载全部配置
-        //     {
-        //         yield return item;
-        //     }
-        //
-        //     // 单个表单个数据
-        //     LogF8.Log(FF8.Config.GetSheet1ByID(2).name);
-        //
-        //     // 单个表全部数据
-        //     foreach (var item in FF8.Config.GetSheet1())
-        //     {
-        //         LogF8.Log(item.Key);
-        //         LogF8.Log(item.Value.name);
-        //     }
-        //
-        //     // 指定名字加载
-        //     Sheet1 sheet1 = new Sheet1();
-        //     sheet1 = FF8.Config.Load<Sheet1>("Sheet1");
-        //     LogF8.Log(sheet1.Dict[2].name);
-        // }
+        IEnumerator Start()
+        {
+            // 同步加载全部配置
+            FF8.Config.LoadAll();
+        
+            foreach (var item in FF8.Config.LoadAllAsync()) // 异步加载全部配置
+            {
+                yield return item;
+            }
+        
+            // 单个表单个数据
+            LogF8.Log(FF8.Config.GetSheet1ByID(2).name);
+        
+            // 单个表全部数据
+            foreach (var item in FF8.Config.GetSheet1())
+            {
+                LogF8.Log(item.Key);
+                LogF8.Log(item.Value.name);
+            }
+        
+            // 指定名字加载
+            Sheet1 sheet1 = new Sheet1();
+            sheet1 = FF8.Config.Load<Sheet1>("Sheet1");
+            LogF8.Log(sheet1.Dict[2].name);
+        }
 
         // // 方式二：运行时读取Excel
         // IEnumerator Start()
